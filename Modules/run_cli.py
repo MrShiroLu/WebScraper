8a9d6e9543b95a0db9eb77e3e53a25a0dd58c501
Burak Artun <artunb2005@gmail.com>
2025-07-25 01:25:22 +0300

    Add URL search functionality in run_cli.py and create initial scrape results file

--- Modules/run_cli.py
+++ Modules/run_cli.py
@@ -2,6 +2,7 @@ import scraper as sc
 import greeting as op
 import sys
 import requests
+import time
 
 try:
     url = sys.argv[1]
@@ -21,3 +22,16 @@ except requests.exceptions.RequestException as err:
     print(f"+"*10+" ERROR "+"+"*10)
     print(f"Message: {err}")
     print(f"+"*10+" ERROR "+"+"*10)
+
+ask = input("Do you want to find a url? (yes/no): ")
+
+if ask == 'yes':
+    
+    word = input("Enter your word: ")
+    finds = [find_url for find_url in result if word in find_url.lower()]
+    
+    for url in finds:
+        print(url)
+else:
+    print("Thank you for using the scraper!")
+    sys.exit(0)
